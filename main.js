eb36f4968188ca571f511c030e7d920752b05775
MOAZ GAD <moaz.a.gad@gmail.com>
2022-06-17 21:00:51 +0200

    intial commit

--- main.js
+++ main.js
@@ -1,46 +1,32 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://dog.ceo/api/breeds/list/all", true);
-
-function getInfo() {
-  xhr.onreadystatechange = function () {
-    if (this.status == 200 && this.readyState == 4) {
-      var info = JSON.parse(this.responseText);
-      var dataMessages = info.message;
-      var output = "<option>Choose a dog breed</option>";
-      for (var i in dataMessages) {
-        output += `
-            <select>
-            
-            <option>${i}</option>
-            </select>
-            
-            `;
-      }
-      document.getElementById("dogs").innerHTML = output;
-    }
-  };
-  xhr.send();
-}
+const getInfo = async () => {
+  const response = await fetch("https://dog.ceo/api/breeds/list/all");
+  const data = await response.json();
+  createList(data.message);
+};
 getInfo();
-var anotherXHR = new XMLHttpRequest();
-function respondFunc(breed) {
-  anotherXHR.open("GET", `https://dog.ceo/api/breed/${breed}/images`);
-  if (breed != "Choose a dog breed") {
-    dataFunc();
+const createList = (dogLists) => {
+  document.getElementById("dog").innerHTML = `
+  <select onchange="dogImage(this.value)">
+  <option>Choose A Dog</option>
+  ${Object.keys(dogLists).map((dog) => {
+    return `<option> ${dog} </option>`;
+  })}
+ 
+  </select>
+  `;
+};
+//End Making List
+const dogImage = async (image) => {
+  if (image !== "Choose A Dog") {
+    const response = await fetch(`https://dog.ceo/api/breed/${image}/images`);
+    const data = await response.json();
+
+    showImage(data.message);
   }
-}
-function dataFunc() {
-  anotherXHR.onload = function () {
-    if (this.status == 200 && this.readyState == 4) {
-      var res = JSON.parse(this.responseText);
-      createSlideShow(res.message);
-    }
-  };
-  anotherXHR.send();
-}
-function createSlideShow(images) {
+};
+
+const showImage = (images) => {
   document.getElementById(
     "slideshow"
-  ).innerHTML = `<div class = "slide" style = "background-image:url('${images[0]}')"></div>
-  <div class = "slide" style = "background-image:url('${images[0]}')"></div>`;
-}
+  ).innerHTML = `<div class="slide" style = "background-image: url('${images[0]}')"></div>`;
+};
